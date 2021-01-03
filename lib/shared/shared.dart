788fa2102315be7e4d8f34e8e51d3e743665ac9e
Alif Irfandi <alifirfandi79@gmail.com>
2021-01-03 10:30:57 +0700

    Add assets & Splash Page

---
+++ lib/shared/shared.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+part 'theme.dart';
+part 'shared_value.dart';
---
+++ lib/ui/pages/pages.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:movieapp/shared/shared.dart';
+
+part 'splash_page.dart';
